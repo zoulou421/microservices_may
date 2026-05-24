34ed1d126b57332c52c0f0452db84f984f272b11
zoulou421 <bonevybeby@gmail.com>
2026-05-24 20:35:20 +0200

    Initial commit microservices project

---
+++ customer-service/customer-service/src/main/java/com/formationkilo/customer_service/CustomerServiceApplication.java
@@ -0,0 +1,24 @@
+package com.formationkilo.customer_service;
+
+import com.formationkilo.customer_service.entities.Customer;
+import com.formationkilo.customer_service.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		return args ->{
+          customerRepository.save(new Customer(null, "BEBY","bonevybeby@gmail.com"));
+		  customerRepository.save(Customer.builder().name("DIOP").email("diop@gmail.com").build());
+		};
+	}
+}
---
+++ customer-service/customer-service/src/main/java/com/formationkilo/customer_service/entities/Customer.java
@@ -0,0 +1,17 @@
+package com.formationkilo.customer_service.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Customer {
+    @Id
+    @GeneratedValue
+    private Long Id;
+    private String name;
+    private String email;
+}
