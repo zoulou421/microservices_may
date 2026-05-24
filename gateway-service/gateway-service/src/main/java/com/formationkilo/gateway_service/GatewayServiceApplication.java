7446012591ef0ac55ed0ffeeac67e91dde695e3f
zoulou421 <bonevybeby@gmail.com>
2026-05-25 00:56:50 +0200

    add DYNAMIC ROUTES CONFIG

--- gateway-service/gateway-service/src/main/java/com/formationkilo/gateway_service/GatewayServiceApplication.java
+++ gateway-service/gateway-service/src/main/java/com/formationkilo/gateway_service/GatewayServiceApplication.java
@@ -2,6 +2,10 @@ package com.formationkilo.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -10,4 +14,10 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator dynamicRoutes(
+			ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+
+	}
 }
