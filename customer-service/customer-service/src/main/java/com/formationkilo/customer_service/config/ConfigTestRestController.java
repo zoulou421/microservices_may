0d382b7053d849f810081f4b7aadff989c4a8591
zoulou421 <bonevybeby@gmail.com>
2026-06-07 23:18:34 +0200

    POST  pour rafrachir : http://localhost:8081/actuator/refresh

--- customer-service/customer-service/src/main/java/com/formationkilo/customer_service/config/ConfigTestRestController.java
+++ customer-service/customer-service/src/main/java/com/formationkilo/customer_service/config/ConfigTestRestController.java
@@ -2,12 +2,14 @@ package com.formationkilo.customer_service.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class ConfigTestRestController {
 
     @Value("${global.params.p1}")
